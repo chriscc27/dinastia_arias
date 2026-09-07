11329088706a05ec610ef9cebef5ad0b8b180a0a
Christian Coronel <chriscoronelc27@gmail.com>
2026-09-07 16:46:28 -0400

    chore: revert github pages configs for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dinastia_arias/',
   plugins: [react()],
   resolve: {
     alias: {
